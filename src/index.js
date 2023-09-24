974ec103c754f0d6bdfd5ebbdc352c1095b0d4ab
klenilsonrox <klenilsoncoc@gmail.com>
2023-09-24 12:45:19 -0300

    att

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ import routerProducts from "./routes/router.js"
 
 const app = express()
 
-app.use((req, res, next) => {
-    // Definir a origem permitida (http://www.google.com)
-    res.header('Access-Control-Allow-Origin', 'http://www.google.com');
-    // Definir outros cabeçalhos CORS permitidos
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// app.use((req, res, next) => {
+//     // Definir a origem permitida (http://www.google.com)
+//     res.header('Access-Control-Allow-Origin', 'https://rxdvamazon.netlify.app');
+//     // Definir outros cabeçalhos CORS permitidos
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     
-    // Permitir credenciais (cookies)
-    res.header('Access-Control-Allow-Credentials', 'true');
+//     // Permitir credenciais (cookies)
+//     res.header('Access-Control-Allow-Credentials', 'true');
     
-    // Tratar as solicitações OPTIONS (preflight)
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
+//     // Tratar as solicitações OPTIONS (preflight)
+//     if (req.method === 'OPTIONS') {
+//       res.sendStatus(200);
+//     } else {
+//       next();
+//     }
+//   });
 
 app.use(express.json())
 app.use("/",routerProducts)
