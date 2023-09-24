7ccf2be3325d517a3582ed7d875128895b8f1ef6
klenilsonrox <klenilsoncoc@gmail.com>
2023-09-24 12:52:09 -0300

    att cors

--- src/index.js
+++ src/index.js
@@ -6,23 +6,9 @@ import routerProducts from "./routes/router.js"
 
 const app = express()
 
-// app.use((req, res, next) => {
-//     // Definir a origem permitida (http://www.google.com)
-//     res.header('Access-Control-Allow-Origin', 'https://rxdvamazon.netlify.app');
-//     // Definir outros cabeçalhos CORS permitidos
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-//     // Permitir credenciais (cookies)
-//     res.header('Access-Control-Allow-Credentials', 'true');
-    
-//     // Tratar as solicitações OPTIONS (preflight)
-//     if (req.method === 'OPTIONS') {
-//       res.sendStatus(200);
-//     } else {
-//       next();
-//     }
-//   });
+app.use(cors({
+    origin:"https://rxdvamazon.netlify.app"
+}))
 
 app.use(express.json())
 app.use("/",routerProducts)
