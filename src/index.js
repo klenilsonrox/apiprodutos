47ba76322436269db69d0ae282e7b5c1feddfca8
klenilsonrox <klenilsoncoc@gmail.com>
2023-09-24 12:32:12 -0300

    update cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import routerProducts from "./routes/router.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:3000/api/produtos"
+    origin:"http://www.google.com"
 }))
 app.use(express.json())
 app.use("/",routerProducts)
